2d9c87aff91aea86cb7692f9177d520d723b5b1d
cupofdirtfordinner <cupofdirtfordinner@gmail.com>
2023-05-01 22:41:02 -0500

    asdfa

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function textclick1() {
-document.getElementById("cumtown").src = "https://raw.githubusercontent.com/cupofdirtfordinner/penis-world/main/penis_mainland_text.png";
+document.getElementById("cumtown").src = "penis/penis_mainland_text.png";
+}
+
+function clickreplace2() {
+document.getElementById("houzedispslay").src = "house/house_living_room.png";
 }
