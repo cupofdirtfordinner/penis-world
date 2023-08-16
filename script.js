25c846a07ab229b66c12e8be9d6ec58e13562c6c
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2023-08-15 21:09:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function textclick1() {
 document.getElementById("cumtown").src = "penis/penis_mainland_text.png";
 }
-
+var counter = 0
 function clickreplace2() {
-document.getElementById("houzedispslay").src = "house/house_living_room.png";
+if (counter = 0) {
+      document.getElementById("houzedispslay").src = "house/house_living_room.png";
+  }
+if (counter = 1) {
+    document.getElementById("houzedispslay").src = "house/house_end.png";
+  }
+if (counter = 2) {
+    document.location.href="penis2.html";
+  }
+  counter += 1
 }
